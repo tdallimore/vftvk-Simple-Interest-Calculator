7e1adc895ee180df03f82ecce0f9d3d495e311a3
Thomas Dallimore <tdallimore@telus.net>
2021-04-16 11:54:33 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+//Function for calculating interest
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+    //Check principal amount entered is greater than 0
+    if (principal < 1) {
+        alert("Enter a positive number");       
+        document.getElementById("principal").focus();
+
+    //If principal amount entered is greater than 0 then show result
+    } else {
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    }
 }
-        
+
+//Function shows interest rate chosen from range slider
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
